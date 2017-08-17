2bdd49446c4850788b6f29df08e1eda7750e4bb8
user <user@user-HP>
2017-08-17 15:15:22 +0900

    두번째 커뮛 B야 내가 손좀 봐 줬다

--- zJAVA_git/src/ch01/BHelloGit.java
+++ zJAVA_git/src/ch01/BHelloGit.java
@@ -3,5 +3,6 @@ package ch01;
 public class BHelloGit {
 	public static void main(String[] args) {
 		System.out.println("B HelloGit First!!!~!~!~!");
+		System.out.println("나 A인데 이걸 코딩이라고 해놨냐");
 	}
 }
