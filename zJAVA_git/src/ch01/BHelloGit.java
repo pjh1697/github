266e41c1ad0c204ec9da60b9e15a3c74615de8cd
user <user@user-HP>
2017-08-17 15:29:00 +0900

    폭탄 받아라

--- zJAVA_git/src/ch01/BHelloGit.java
+++ zJAVA_git/src/ch01/BHelloGit.java
@@ -4,5 +4,6 @@ public class BHelloGit {
 	public static void main(String[] args) {
 		System.out.println("B HelloGit First!!!~!~!~!");
 		System.out.println("나 A인데 이걸 코딩이라고 해놨냐");
+		System.out.println("웃기고있네?? 발로 코딩했냐");
 	}
 }
